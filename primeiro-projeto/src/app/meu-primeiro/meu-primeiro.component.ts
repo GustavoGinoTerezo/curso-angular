c3efe800797f65b3aaa1fa351a69aa1c252fff99
Gustavo Gino Terezo <gustavogino@outlook.com.br>
2023-03-27 19:17:16 -0300

    Update meu-primeiro.component.ts

--- primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component( {
   selector: 'meu-primeiro-component',
   template: `
-    <p>Meu primeiro componenet com Angula 2!</p>
+    <p>Meu primeiro component com Angula 2!</p>
   `
 } )
 export class MeuPrimeiroComponent { }
