73bdaeb63ca0358d312934e42f0d5401435e3b36
Gustavo Gino Terezo <gustavogino@outlook.com.br>
2023-03-28 23:10:34 -0300

    Update button.component.spec.ts

--- segundo-projeto/src/app/button/button.component.spec.ts
+++ segundo-projeto/src/app/button/button.component.spec.ts
@@ -1,4 +1,4 @@
-/*
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ButtonComponent } from './button.component';
@@ -22,4 +22,4 @@ describe('ButtonComponent', () => {
     expect(component).toBeTruthy();
   });
 });
-*/
+
