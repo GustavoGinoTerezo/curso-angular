7c58aece8ceb11e02df9c7d9036053e9daa763f8
Gustavo Gino Terezo <gustavogino@outlook.com.br>
2023-04-04 23:02:26 -0300

    Update
    Consumo total da API nova que o professor passou.

--- projeto-solicitacao-bd/src/app/componentes/form-field/form-field.component.ts
+++ projeto-solicitacao-bd/src/app/componentes/form-field/form-field.component.ts
@@ -18,17 +18,52 @@ export class FormFieldComponent {
   selecionarImagem(event: any): void {
     this.img = event.target.files;
   }
+
+//==================================================================================================================//
+//==================================================================================================================//
+  //post
+  async criarDado(){
+
+    const url = "https://crudcrud.com/api/7dab6c99d39d479e8919652c1a51a6d8/unicorns";
+
+    /* não utilizei pois mudei o método para passar as informações
+    const formData = new FormData();
+      formData.append("nome", this.nome )
+      formData.append("email", this.email )
+      formData.append("senha", this.senha )
+      formData.append("imagem", this.img);
+    */
+
+    //axios.post(url, formData) => como era
+
+    await axios.post(url, { // = > como ficou
+      nome: this.nome,
+      email: this.email,
+      senha: this.senha
+    })
+      .then(function(response){
+        console.log("Funcionando", response);
+      })
+      .catch(function (error){
+        console.error("Erro", error);
+      });
+  }
 //==================================================================================================================//
 //==================================================================================================================//
   //get
   async pegarDado(){
 
-    const url = "https://jsonplaceholder.typicode.com/posts/" + this.id;
+    const url = "https://crudcrud.com/api/7dab6c99d39d479e8919652c1a51a6d8/unicorns/" + this.id;
+    //id = 642ccc67308c9903e84ec35a
 
     await axios.get(url)
       .then(response =>{
         console.log("Recebido", response.data);
-        this.nome = response.data.title;
+        this.nome = response.data.nome //usado na API unicorn
+        this.email = response.data.email
+        this.senha =  response.data.senha
+
+        //this.nome = response.data.title; => usado na API do professor
         //this.email = response.data.email;
         //this.senha = response.data.senha;
       })
@@ -40,39 +75,24 @@ export class FormFieldComponent {
   }
 //==================================================================================================================//
 //==================================================================================================================//
-  //post
-  criarDado(){
-
-    const url = "https://rhuna.herokuapp.com/crud";
-
-    const formData = new FormData();
-      formData.append("nome", this.nome )
-      formData.append("email", this.email )
-      formData.append("senha", this.senha )
-      formData.append("imagem", this.img);
-
-    axios.post(url, formData)
-      .then(function(response){
-        console.log("Funcionando", response);
-      })
-      .catch(function (error){
-        console.error("Erro", error);
-      });
-  }
-//==================================================================================================================//
-//==================================================================================================================//
   //put
   atualizarDado(){
 
-    const url = "https://rhuna.herokuapp.com/crud" + this.id;
+    const url = "https://crudcrud.com/api/7dab6c99d39d479e8919652c1a51a6d8/unicorns/" + this.id;
 
+    /*
     const formData = new FormData();
       formData.append("nome", this.nome )
       formData.append("email", this.email )
       formData.append("senha", this.senha )
       formData.append("imagem", this.img);
+    */
 
-    axios.put(url, formData)
+    axios.put(url, {
+      nome: this.nome,
+      email: this.email,
+      senha: this.senha
+    })
       .then(response => {
         console.log ("Resposta recebida", response.data);
       })
@@ -85,7 +105,7 @@ export class FormFieldComponent {
   //delete
   deletarDado(){
 
-    const url = "https://rhuna.herokuapp.com/crud/delete/" + this.id;
+    const url = "https://crudcrud.com/api/7dab6c99d39d479e8919652c1a51a6d8/unicorns/" + this.id;
 
     axios.delete(url)
     .then(response => {
